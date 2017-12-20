480e1f8a0c8ad4e801b0d0165340ceb8f97bf42f
cherrywoods <cherrywoods@posteo.org>
2017-12-20 21:21:22 +0100

    Made framework usable

--- Sources/NoSerialization.swift
+++ Sources/NoSerialization.swift
@@ -8,28 +8,30 @@
 import Foundation
 import MetaSerialization
 
+/**
+ This serialization class converts swift objects to Metas using another translator.
+ One can say, that this serialization class will just wrap or unwrap the swift object and not encode or decode it.
+ */
 public class ToMetaSerialization: Serialization {
     
     public typealias Raw = Meta
     
-    private let translator: Translator
+    private let noTranslator: NoTranslator
     
     public func provideNewEncoder() -> MetaEncoder {
         
-        return MetaEncoder(translator: translator)
+        return MetaEncoder(translator: noTranslator)
         
     }
     
     public func provideNewDecoder(raw: Meta) -> MetaDecoder {
         
-        return try! MetaDecoder(translator: translator, raw: raw)
+        return try! MetaDecoder(translator: noTranslator, raw: raw)
         
     }
     
-    public init(translator: Translator = NoTranslator()) {
-        
-        self.translator = translator
-        
+    public init(translator: Translator) {
+        self.noTranslator = NoTranslator(wrapping: translator)
     }
     
 }
--- Sources/NoTranslator.swift
+++ Sources/NoTranslator.swift
@@ -8,28 +8,40 @@
 import Foundation
 import MetaSerialization
 
-/// This translator does nothing. It translates to and from Metas.
+/**
+ This translator encodes/decodes to/from Metas.
+ It wrappes another translator to wrap and unwrap swift objects,
+ but will not perform any encoding or decoding process,
+ it will jsut return the constructed Meta.
+ 
+ Therefor you may use this translator to construct
+ only the meta representation for an arbitrary other translator.
+ */
 public class NoTranslator: Translator {
     
-    public init() {}
+    public let wrappedTranslator: Translator
     
-    public func wrapingMeta<T>(for value: T) -> Meta? {
+    public init(wrapping translator: Translator) {
         
-        if T.self == GenericNil.self {
-            return NilMeta.nil
-        }
-        
-        return SimpleGenericMeta<T>() as Meta?
+        self.wrappedTranslator = translator
         
     }
     
-    public func unwrap<T>(meta: Meta) -> T {
+    // MARK: wrapingMeta and unwrap
+    
+    public func wrappingMeta<T>(for value: T) -> Meta? {
         
-        // meta will be initalized (get() will not return nil)
-        // and the value should be of type T.
-        return meta.get() as! T
+        return wrappedTranslator.wrappingMeta(for: value)
         
     }
+    
+    public func unwrap<T>(meta: Meta) throws -> T? {
+        
+        return try wrappedTranslator.unwrap(meta: meta)
+        
+    }
+    
+    // MARK: encode and decode
     
     public func encode<Raw>(_ meta: Meta) throws -> Raw {
         precondition(Raw.self == Meta.self, "Incorrect translation type")
